a3fb8b17861de293da36224cc6570e1355d3fd76
Trey Waller <tlwaller99@gmail.com>
2022-01-04 14:48:31 -0600

    converted app to stateful component and expense list now dynamically renders.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,26 +16,17 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {
+          props.items.map((e, i) => {
+            return (
+            <ExpenseItem
+              title={e.title}
+              amount={e.amount}
+              date={e.date}
+            />
+            )
+          })
+        }
       </Card>
     </div>
   );
